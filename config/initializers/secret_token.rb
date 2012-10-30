c62139776e62d4b1abc3f8f52eaa54dc0695db0f
bcolner <bcolner@ngmoco.com>
2012-10-30 00:05:27 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuHellp::Application.config.secret_token = '5cc1e55871426e51d0ab4cedd66baf5df190152b1190738681471a265f0ed95648239b66b121060d2942d75777793ea39fe982f18e8f35ca248e8e7e9f26f495'
